f0fd923a11557a5c1ed8034f80cb05ece3188982
Andre <andre.torres@capco.com>
2020-04-16 13:15:39 -0300

    Adicionando action para recuperar filmes via HTTP

---
+++ CopaFilmesAPI/Controllers/CopaController.cs
@@ -0,0 +1,23 @@
+﻿using CopaFilmesAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CopaFilmesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CopaController : ControllerBase
+    {
+        private readonly IFilmesRepository _repository;
+
+        public CopaController(IFilmesRepository repository){
+            _repository = repository;
+        }
+
+        [HttpGet("filmes")]
+        public JsonResult GetAllFilmes()
+        {
+            return new JsonResult(_repository.GetAll());
+        }
+
+    }
+}

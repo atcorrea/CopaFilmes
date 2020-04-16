c75e0e16c0f0121aab84b9a4e319b41a9e778265
Andre <andre.torres@capco.com>
2020-04-16 16:50:43 -0300

    adicionando action para disputar copa

--- CopaFilmesAPI/Controllers/CopaController.cs
+++ CopaFilmesAPI/Controllers/CopaController.cs
@@ -1,4 +1,6 @@
-﻿using CopaFilmesAPI.Repository;
+﻿using System.Collections.Generic;
+using CopaFilmesAPI.Models;
+using CopaFilmesAPI.Repository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CopaFilmesAPI.Controllers
@@ -17,6 +19,17 @@ namespace CopaFilmesAPI.Controllers
         public JsonResult GetAllFilmes()
         {
             return new JsonResult(_repository.GetAll());
+        }
+
+        [HttpPost("disputar")]
+        public JsonResult DisputarCopa([FromBody] List<Filme> filmes)
+        {
+            var copa = new Copa();
+            var competidores = copa.CadastrarCompetidores(filmes);
+            var partidas = copa.PrepararPartidas();
+            var final = copa.DisputarCopa(partidas);
+
+            return new JsonResult(new {primeiro = final.Vencedor, segundo = final.Perdedor});
         }
 
     }
